4f686ac0f80caead25a77bfc07257deb988e138d
bakovetskaaa <olyabakovetska@gmail.com>
2023-04-19 21:56:58 +0300

    hw3-ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import {useState} from 'react';
+
+import {PageEnum} from './constants/page.enum';
+import {UserPage} from './pages/UserPage';
+import {CommentPage} from './pages/CommentPage';
+import {CarPage} from './pages/CarPage';
+import {Header} from './components/Header/Header';
 
 const App = () => {
-  return (
-      <div>
-        
-      </div>
-  );
+    const [choice, setChoice] = useState<PageEnum>(PageEnum.USERS);
+    return (
+        <div>
+            <Header setChoice={setChoice}/>
+            {choice === PageEnum.USERS && <UserPage/>}
+            {choice === PageEnum.COMMENTS && <CommentPage/>}
+            {choice === PageEnum.CARS && <CarPage/>}
+        </div>
+    );
 };
 
 export default App;
