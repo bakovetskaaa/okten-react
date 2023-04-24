0aaacd445d9950195e1446bc255ef7e3bf723dc9
bakovetskaaa <olyabakovetska@gmail.com>
2023-04-24 22:26:45 +0300

    hw4

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import './App.css';
+import {Navigate, Route, Routes} from "react-router-dom";
+import {MainLayout} from "./layouts/MainLayout";
+import {TodosPage} from "./pages/TodosPage/TodosPage";
+import {AlbumsPage} from "./pages/AlbumsPage/AlbumsPage";
+import {CommentsPage} from "./pages/CommentsPage/CommentsPage";
+import {Post} from "./components/Post/Post";
 
-function App() {
-    let msg = 'Hello!';
-
-            return (
-              <div>
-               <h1>{msg}</h1>
-
-
-              </div>
-            );
-}
+const App = () => {
+    return (
+        <Routes>
+            <Route path={'/'} element={<MainLayout/>}>
+                <Route index element={<Navigate to={'todos'}/>}/>
+                <Route path={'todos'} element={<TodosPage/>}/>
+                <Route path={'albums'} element={<AlbumsPage/>}/>
+                <Route path={'comments'} element={<CommentsPage/>}>
+                    <Route path={':postId'} element={<Post/>}/>
+                </Route>
+            </Route>
+        </Routes>
+    );
+};
 
 export default App;
---
+++ src/pages/AlbumsPage/AlbumsPage.js
@@ -0,0 +1,12 @@
+
+import {Albums} from "../../components/Albums/Albums";
+
+const AlbumsPage = () => {
+    return (
+        <div>
+            <Albums/>
+        </div>
+    );
+};
+
+export {AlbumsPage};
