a1d7b73c0e846cd2b26b1e6177b63d46c2f980e0
bakovetskaaa <olyabakovetska@gmail.com>
2023-05-03 14:12:39 +0300

    hw5

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import './App.css';
+import {Navigate, Route, Routes} from "react-router-dom";
+import {MainLayout} from "./layouts/MainLayout";
+import {PostPage} from "./pages/PostPage";
+import {CommentPage} from "./pages/CommentPage";
+import {CatDogPage} from "./pages/CatDogPage";
 
-function App() {
-    let msg = 'Hello!';
-
-            return (
-              <div>
-               <h1>{msg}</h1>
-
-
-              </div>
-            );
-}
+const App = () => {
+    return (
+        <Routes>
+            <Route path={'/'} element={<MainLayout/>}>
+                <Route index element={<Navigate to={'posts'}/>}/>
+                <Route path={'posts'} element={<PostPage/>}/>
+                <Route path={'comments'} element={<CommentPage/>}/>
+                <Route path={'cats_dogs'} element={<CatDogPage/>}/>
+            </Route>
+        </Routes>
+    );
+};
 
 export default App;
